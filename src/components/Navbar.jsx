b3374844fca9bea4e1baeef0028cd48b8a94192c
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-11 13:40:40 +0530

    handlescroll function created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import useMediaQuery from "../hooks/useMediaQuery";
+
 import AnchorLink from "react-anchor-link-smooth-scroll";
+
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const Link = ({ page, selectedPage, setSelectedPage }) => {
     const loweCasePage = page?.toLowerCase();
@@ -17,11 +19,13 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
     );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-red";
+
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6 `}>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6 `}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-playfair text-3xl font-bold">HB</h4>
 
@@ -33,9 +37,81 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
                             selectedPage={selectedPage}
                             setSelectedPage={setSelectedPage}
                         />
+                        <Link
+                            page="Skills"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Projects"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Testimonials"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Contact"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
                     </div>
                 ) : (
-                    <div></div>
+                    <button
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        className="rounded-full bg-red p-2"
+                    >
+                        <img
+                            src="../assets/menu-icon.svg"
+                            alt="menu-icon"
+                        />
+                    </button>
+                )}
+
+                {/* MOBILE MENU */}
+                {!isAboveSmallScreens && isMenuToggled && (
+                    <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                        {/* CLOSE ICON */}
+                        <div className="flex justify-end p-12">
+                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <img
+                                    src="../assets/close-icon.svg"
+                                    alt="close-icon"
+                                />
+                            </button>
+                        </div>
+
+                        {/* MENU ITEMS  */}
+                        <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                            <Link
+                                page="Home"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Skills"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Projects"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Testimonials"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Contact"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                        </div>
+                    </div>
                 )}
             </div>
         </nav>

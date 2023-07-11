17be9cb77383be97eb10c3e5962f63a179e25a2f
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-11 15:38:35 +0530

    added framer-motion animations

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -52,6 +52,45 @@ const Landing = ({ setSelectedPage }) => {
                         ex officia incidunt doloribus sint maxime praesentium.
                     </p>
                 </motion.div>
+
+                {/* ACTIONS */}
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}
+                >
+                    <AnchorLink
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+                        hover:bg-blue hover:text-white transition duration-500"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        Contact Me
+                    </AnchorLink>
+
+                    <AnchorLink
+                        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10">
+                            Let's talk.
+                        </div>
+                    </AnchorLink>
+                </motion.div>
+
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}
+                >
+                    <SocialMedia />
+                </motion.div>
             </div>
         </section>
     );
